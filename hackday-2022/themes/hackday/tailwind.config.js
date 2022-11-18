2cc0129776a5c65f0a1fe27fa6237ff8ba5a558c
David Weid II <david.weid.2@gmail.com>
2022-11-18 10:01:18 -0500

    add tailwind

---
+++ hackday-2022/themes/hackday/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.md",
+    "./layouts/**/*.html",
+    "./component-library/**/*.html",
+    "./themes/hackday/layouts/**/*.html",
+    "./themes/hackday/content/**/*.md"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};

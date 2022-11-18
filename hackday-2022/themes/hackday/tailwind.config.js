78750edf702e9cf70a265e1e78f5293494669121
David Weid II <david.weid.2@gmail.com>
2022-11-18 10:36:05 -0500

    more testing of tailwind

--- hackday-2022/themes/hackday/tailwind.config.js
+++ hackday-2022/themes/hackday/tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: [
     "./content/**/*.md",
-    "./layouts/**/*.html",
-    "./component-library/**/*.html",
-    "./themes/hackday/layouts/**/*.html",
-    "./themes/hackday/content/**/*.md"
+    "./layouts/**/*.{html,md}",
+    "./component-library/**/*.{html,md}",
+    "./component-library/components/hero/*.html",
+    "./themes/hackday/layouts/**/*.{html,md}",
+    "./themes/hackday/content/**/*.md",
+    "../../component-library/**/*.{html,md}",
+    "../../component-library/components/hero/*.html",
+    "../../content/**/*.md",
+    "../../layouts/**/*.{html,md}",
+    "../../themes/hackday/layouts/**/*.{html,md}",
+    "../../themes/hackday/content/**/*.md"
   ],
   theme: {
     extend: {}
